eef2e872592372e77db198aa13681a36617bb346
Mauro <mauroevangelista08@gmail.com>
2022-10-30 23:35:51 -0300

    Inicio del proyecto

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <div clasName="comment">
+                <a href>
+
+
+                </a>
+
+
+            </div>
+
+
+        </div>
+
+    )
+}
+
+ReactDOM.render(<App />,document.querySelector('#root'));
