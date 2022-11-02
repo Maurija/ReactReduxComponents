c1c593ef032aa7f884b16b9695a50b874bf88dad
Mauro Evangelista <mauroevangelista08@gmail.com>
2022-11-02 20:30:43 -0300

    Nuevo componente Comentario

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import CommentDetail from './CommentDetail';
+import {faker} from '@faker-js/faker';
 
 const App = () => {
     return (
+        
         <div className="ui container comments">
-            <div clasName="comment">
-                <a href >
- 
-
-                </a>
-
-
-            </div>
+            
+            <CommentDetail 
+                author="Sam" 
+                timeAgo="Today at 4:45PM" 
+                comment="Comentario1" 
+                avatar={faker.image.avatar()}
+             />
+            <CommentDetail 
+                author="Alex" 
+                timeAgo="Yesterday at 6:00PM" 
+                comment="Comentario2" 
+                avatar={faker.image.avatar()}
+                />
+            
 
 
         </div>
+
 
     )
 }
