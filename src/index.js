24afc7b207b6f097a3bea9cfc0d3553269973f92
Mauro Evangelista <mauroevangelista08@gmail.com>
2022-11-01 21:08:26 -0300

    Commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const App = () => {
     return (
         <div className="ui container comments">
             <div clasName="comment">
-                <a href>
+                <a href >
 
 
                 </a>
